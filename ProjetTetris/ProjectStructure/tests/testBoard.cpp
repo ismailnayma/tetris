1975860d937db01cf29d124ee32891d7e3483f44
60085@etu.he2b.be <60085@etu.he2b.be>
2024-03-16 12:45:22 +0100

    test drop current brick, empty boardArea

--- ProjetTetris/ProjectStructure/tests/testBoard.cpp
+++ ProjetTetris/ProjectStructure/tests/testBoard.cpp
@@ -296,60 +296,26 @@ TEST_CASE("Rotate current Brick collision", "[board]") {
 
 
 
+//DROP
 
-
-/*
-TEST_CASE("Move current brick", "[board]") {
-    // Création d'un plateau de jeu rempli et une brique courante
+TEST_CASE("Drop current Brick, empty boardArea", "[board]") {
     Board board;
-    for(int i = 0; i<2; i++){
-        for(int j = 0; j<board.getBoardWidth()-1; ++j){
-            board.getBoardArea()[board.getBoardHeight()-i-1][j] = TypeShape::I_SHAPE;
-        }
-    }
-
-    Brick brick(TypeShape::L_SHAPE, Orientation::DOWN, Position(9, 19));
+    Brick brick(TypeShape::L_SHAPE, Orientation::UP, Position(0, 1)); //we start at the top
     board.setCurrentBrick(brick);
+    REQUIRE(board.dropCurrentBrick() == 17); //drop distance should be 17
 
-    REQUIRE(board.moveCurrentBrick(Direction::LEFT) == false); // Check if the brick can be translated to the left
 
-
-    REQUIRE_FALSE(cell.has_value());
+    //check that the brick was successfully dropped
     auto boardArea = board.getBoardArea();
+    REQUIRE(boardArea[17][0].value() == TypeShape::L_SHAPE);
+    REQUIRE(boardArea[18][0].value() == TypeShape::L_SHAPE);
+    REQUIRE(boardArea[19][0].value() == TypeShape::L_SHAPE);
+    REQUIRE(boardArea[19][1].value() == TypeShape::L_SHAPE);
 
-    Brick initialBrick(TypeShape::L_SHAPE, Orientation::UP, Position(5, 0)); // Placer la brique initialement en (5, 0)
-    board.setCurrentBrick(initialBrick);
-
-    SECTION("Move right") {
-        REQUIRE(board.moveCurrentBrick(Direction::RIGHT) == true); // Déplacer la brique vers la droite
-        // Vérifier si la brique a été déplacée correctement vers la droite sur le plateau
-        auto boardArea = board.getBoardArea();
-        REQUIRE(boardArea[0][5].value() == TypeShape::L_SHAPE);
-        REQUIRE(boardArea[1][5].value() == TypeShape::L_SHAPE);
-        REQUIRE(boardArea[1][6].value() == TypeShape::L_SHAPE);
-        REQUIRE(boardArea[1][7].value() == TypeShape::L_SHAPE);
-    }
-
-    SECTION("Move left") {
-        REQUIRE(board.moveCurrentBrick(Direction::LEFT) == true); // Déplacer la brique vers la gauche
-        // Vérifier si la brique a été déplacée correctement vers la gauche sur le plateau
-        auto boardArea = board.getBoardArea();
-        REQUIRE(boardArea[0][3].value() == TypeShape::L_SHAPE);
-        REQUIRE(boardArea[1][3].value() == TypeShape::L_SHAPE);
-        REQUIRE(boardArea[1][4].value() == TypeShape::L_SHAPE);
-        REQUIRE(boardArea[1][5].value() == TypeShape::L_SHAPE);
-    }
-
-    SECTION("Move down") {
-        REQUIRE(board.moveCurrentBrick(Direction::DOWN) == true); // Déplacer la brique vers le bas
-        // Vérifier si la brique a été déplacée correctement vers le bas sur le plateau
-        auto boardArea = board.getBoardArea();
-        REQUIRE(boardArea[1][5].value() == TypeShape::L_SHAPE);
-        REQUIRE(boardArea[2][5].value() == TypeShape::L_SHAPE);
-        REQUIRE(boardArea[2][6].value() == TypeShape::L_SHAPE);
-        REQUIRE(boardArea[2][7].value() == TypeShape::L_SHAPE);
-    }
-
+    //check the currentBrick's parameters, its board position is supposed to change
+    REQUIRE(board.currentBrick.getBoardPosition() ==  Position(0,18));
 }
-*/
+
+
+
 
